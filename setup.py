de19f4c878a1609326dc51cbb0744a84c1139cc6
divy9881 <divy9881@gmail.com>
2020-07-15 19:21:36 +0530

    fix: bump version 1.0.0 -> 1.1.0
    Signed-off-by: divy9881 <divy9881@gmail.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setup(
     name="image-pcd",
-    version="1.0.0",
+    version="1.1.0",
     description="A engine which detects prominent colors from the image.",
     long_description=README,
     long_description_content_type="text/markdown",
